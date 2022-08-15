d3315d9bbf6252e91b3a02b73309071af6241a72
Anmy Moreno <nayle2701@gmail.com>
2022-08-15 10:06:15 -0400

    start

--- src/componetes/Footer.jsx
+++ src/componetes/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='container_Footer'>
         <div className="info">
-            <h2>Developed By Jesus Arrechider and Anmy Moreno</h2>
+            <h2>Developed By Anmy Moreno</h2>
         </div>
         <div className="footer_social">
             <a href=""><i class='bx bxl-github'></i></a>
